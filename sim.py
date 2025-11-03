55fd064515b8bcc1e37bd6471a7f2e192b86dcc7
jlijewski <jlijewski@ufl.edu>
2025-11-03 14:43:36 -0500

    organized files

--- sim.py
+++ sim.py
@@ -1,32 +1,44 @@
-import os
-import sys
-import traci
+# Step 1: Add modules to provide access to specific libraries and functions
+import os # Module provides functions to handle file paths, directories, environment variables
+import sys # Module provides access to Python-specific system parameters and functions
 
-
-
+# Step 2: Establish path to SUMO (SUMO_HOME)
 if 'SUMO_HOME' in os.environ:
-    # Set up the system path for TraCI
-    sys.path.append(os.path.join(os.environ['SUMO_HOME'], 'tools'))
-    sumo_binary = os.path.join(os.environ['SUMO_HOME'], 'bin', 'sumo-gui.exe')
+    tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
+    sys.path.append(tools)
 else:
-    sys.exit("Please declare the environment variable 'SUMO_HOME'")
+    sys.exit("Please declare environment variable 'SUMO_HOME'")
 
+# Step 3: Add Traci module to provide access to specific libraries and functions
+import traci # Static network information (such as reading and analyzing network files)
 
-sumo_cfg = 'simulation.sumocfg'
+# Step 4: Define Sumo configuration
+Sumo_config = [
+    'sumo-gui',
+    '-c', 'Traci.sumocfg',
+    '--step-length', '0.05',
+    '--delay', '1000',
+    '--lateral-resolution', '0.1'
+]
 
+# Step 5: Open connection between SUMO and Traci
+traci.start(Sumo_config)
 
-sumo_binary = "sumo-gui"
-sumo_cmd = [sumo_binary, "-c", sumo_cfg]
+# Step 6: Define Variables
+vehicle_speed = 0
+total_speed = 0
 
-#
-try:
-    traci.start(sumo_cmd)
-    step = 0
-    while traci.simulation.getMinExpectedNumber() > 0:
-        traci.simulationStep()
-  
-        # vehicle_speed = traci.vehicle.getSpeed('vehicle_id')
-        step += 1
-finally:
-    traci.close()
-    sys.stdout.flush()
+# Step 7: Define Functions
+
+# Step 8: Take simulation steps until there are no more vehicles in the network
+while traci.simulation.getMinExpectedNumber() > 0:
+    traci.simulationStep() # Move simulation forward 1 step
+    # Here you can decide what to do with simulation data at each step
+    if 'veh1' in traci.vehicle.getIDList():
+        vehicle_speed = traci.vehicle.getSpeed('veh1')
+        total_speed = total_speed + vehicle_speed
+    # step_count = step_count + 1
+    print(f"Vehicle speed: {vehicle_speed} m/s")
+
+# Step 9: Close connection between SUMO and Traci
+traci.close()

10484ca73e1d57144114f7f54eb020f0b881929b
Caden Joergens <58443587+StellarRaccoon@users.noreply.github.com>
2025-11-05 23:09:55 -0500

    py formatting

--- .history/sim_20251105225245.py
+++ .history/sim_20251105225245.py
@@ -1,31 +1,27 @@
 # Step 1: Add modules to provide access to specific libraries and functions
-import os  # Module provides functions to handle file paths, directories, environment variables
-import sys  # Module provides access to Python-specific system parameters and functions
+import os # Module provides functions to handle file paths, directories, environment variables
+import sys # Module provides access to Python-specific system parameters and functions
 
 from vehicle import Vehicle
 import random
 
 # Step 2: Establish path to SUMO (SUMO_HOME)
-if "SUMO_HOME" in os.environ:
-    tools = os.path.join(os.environ["SUMO_HOME"], "tools")
+if 'SUMO_HOME' in os.environ:
+    tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
     sys.path.append(tools)
 else:
     sys.exit("Please declare environment variable 'SUMO_HOME'")
 
 # Step 3: Add Traci module to provide access to specific libraries and functions
-import traci  # Static network information (such as reading and analyzing network files)
+import traci # Static network information (such as reading and analyzing network files)
 
 # Step 4: Define Sumo configuration
 Sumo_config = [
-    "sumo-gui",
-    "-c",
-    "highway/highway.sumocfg",
-    "--step-length",
-    "0.05",
-    "--delay",
-    "1000",
-    "--lateral-resolution",
-    "0.1",
+    'sumo-gui',
+    '-c', 'highway/highway.sumocfg',
+    '--step-length', '0.05',
+    '--delay', '1000',
+    '--lateral-resolution', '0.1'
 ]
 
 # Step 5: Open connection between SUMO and Traci
@@ -42,36 +38,38 @@ changeLaneTest = False
 
 # Step 8: Take simulation steps until there are no more vehicles in the network
 while traci.simulation.getMinExpectedNumber() > 0:
-    traci.simulationStep()  # Move simulation forward 1 step
+    traci.simulationStep() # Move simulation forward 1 step
     # Here you can decide what to do with simulation data at each step
-    if random.randint(1, 75) == 7:
+    if(random.randint(1, 75) == 7):
         changeLaneTest = True
 
+   
     for newVeh in traci.simulation.getDepartedIDList():
-        vehicles[newVeh] = Vehicle(
-            newVeh,
-            traci.vehicle.getSpeed(newVeh),
-            traci.vehicle.getAcceleration(newVeh),
-            traci.vehicle.getPosition(newVeh),
-            traci.vehicle.getLaneIndex(newVeh),
-            traci.vehicle.getLanePosition(newVeh),
-            traci.vehicle.getLength(newVeh),
-        )
+        vehicles[newVeh] = Vehicle(newVeh,traci.vehicle.getSpeed(newVeh),traci.vehicle.getAcceleration(newVeh),
+                                    traci.vehicle.getPosition(newVeh),traci.vehicle.getLaneIndex(newVeh),
+                                    traci.vehicle.getLanePosition(newVeh),traci.vehicle.getLength(newVeh))
         vehicles[newVeh].disableLaneSwitch(traci)
-
+            
+           
     for currVeh in traci.vehicle.getIDList():
-        if changeLaneTest == True:
-            if vehicles[currVeh].targetLane == -1:
-                if vehicles[currVeh].lane == 4:
+        if(changeLaneTest == True):
+            if(vehicles[currVeh].targetLane == -1):
+                if(vehicles[currVeh].lane == 4):
                     vehicles[currVeh].laneSwitchStart(3)
                 else:
-                    vehicles[currVeh].laneSwitchStart(vehicles[currVeh].lane + 1)
+                    vehicles[currVeh].laneSwitchStart(vehicles[currVeh].lane+1)
+
+
 
             changeLaneTest = False
         vehicles[currVeh].update(traci)
-
+    
     for oldVeh in traci.simulation.getArrivedIDList():
         del vehicles[oldVeh]
+
+
+    
+
 
 
 # Step 9: Close connection between SUMO and Traci
---
+++ .history/vehicle_20251105230557.py
@@ -0,0 +1,104 @@
+from enum import Enum
+from queue import Queue
+
+class VehicleState(Enum):
+   
+    Idle = 1
+    SendingRequest = 2
+    WaitingOnAck = 3
+
+# class Lanes(Enum):   
+#     Zero = "E1_0"
+#     One = "E1_1"
+#     Two = "E1_2"
+#     Three = "E1_3"
+#     Four = "E1_4"
+
+
+
+
+class Vehicle:
+
+    vehicle_requests : dict[str,Queue] = {}
+
+    def __init__(self, name, speed,accel,pos,lane,lanePos,length):
+        self.name = name
+        self.speed = speed
+        self.accel = accel
+        self.pos = pos
+        self.lane = lane
+        self.lanePos = lanePos
+        self.length = length
+        self.targetLane = -1
+        self.state = VehicleState.Idle
+        self.vehicle_requests[self.name] = Queue()
+        self.ackCount = 0
+        #print(f"Created: {name}")
+
+    def __del__(self):
+            print(f"Deleted: {self}")
+
+
+    def disableLaneSwitch(self,traci):
+        traci.vehicle.setLaneChangeMode(self.name,0)
+
+    def update(self,traci):
+        self.speed = traci.vehicle.getSpeed(self.name)
+        self.accel = traci.vehicle.getAcceleration(self.name)
+        self.pos = traci.vehicle.getPosition(self.name)
+        self.lane = traci.vehicle.getLaneIndex(self.name)
+        self.lanePos = traci.vehicle.getLanePosition(self.name)
+        self.length = traci.vehicle.getLength(self.name)
+
+        if(self.state == VehicleState.SendingRequest):#iterate over all cars in target lane and send nearest ones a request
+            for veh in traci.lane.getLastStepVehicleIDs("E1_" + str(self.targetLane)):
+                checkLanePos = traci.vehicle.getLanePosition(veh)
+                if(checkLanePos < (self.lanePos + self.length) and checkLanePos > (self.lanePos - self.length)):
+                    self.vehicle_requests[veh].put(self.name+"/R")
+
+                    self.ackCount += 1    
+            if(self.ackCount == 0):
+                self.laneSwitch(traci)
+            else:
+                self.state = VehicleState.WaitingOnAck
+        elif(self.state == VehicleState.WaitingOnAck):
+            if(self.ackCount == 0):
+                self.laneSwitch(traci)
+            else:
+                self.state = VehicleState.WaitingOnAck
+
+        while not self.vehicle_requests[self.name].empty():
+            item = self.vehicle_requests[self.name].get_nowait()
+            stringParts = item.split('/')
+            if(stringParts[1] == 'R'):
+                self.vehicle_requests[stringParts[0]].put(self.name+"/A")
+            else:
+                self.ackCount -= 1
+
+
+
+         
+
+                 
+                 
+
+
+    def laneSwitchStart(self,target):
+         self.state = VehicleState.SendingRequest
+         print("started switch")
+         self.targetLane = target
+
+    def laneSwitch(self,traci):
+        self.state = VehicleState.Idle
+       
+        print("CHANGED LANE")
+        traci.vehicle.changeLane(
+                    vehID=self.name,
+                    laneIndex=self.targetLane,
+                    duration=2.0
+                )
+        self.targetLane = -1
+
+      
+
+        
